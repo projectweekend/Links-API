f60fafb8037a9d0a1f04fd402f5f3a6ea2c6d621
Brian Hines <brian@projectweekend.net>
2014-07-30 06:04:48 -0500

    Start work on user model

--- links/maker/models.py
+++ links/maker/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class Maker(AbstractBaseUser, PermissionsMixin):
+
+    identifier = models.CharField(max_length=200, unique=True)
+    is_admin = models.BooleanField(default=False)
+    photo_url = models.URLField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True)
+
+    USERNAME_FIELD = 'identifier'
