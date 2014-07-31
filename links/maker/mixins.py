634ce6fe56f44d00bda054f7306a624cdc9f8bf8
Brian Hines <brian@projectweekend.net>
2014-07-31 17:52:59 -0500

    Authenticated maker mixin

--- links/maker/mixins.py
+++ links/maker/mixins.py
@@ -1,13 +1,16 @@
 from maker.models import Maker
-from maker.serializers import MakerReadSerializer
+from maker.serializers import (MakerReadSerializer, MakerUpdateSerializer)
 
 
 class AuthenticatedMaker(object):
-
-    serializer_class = MakerReadSerializer
 
     def get_queryset(self):
         return Maker.objects.get(pk=self.request.user.pk)
 
     def get_object(self):
         return self.get_queryset()
+
+    def get_serializer_class(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return MakerUpdateSerializer
+        return MakerReadSerializer
