52aa16efda8f9d77a6c4c27e1515712609c8ecf1
Brian Hines <brian@projectweekend.net>
2014-07-30 20:28:47 -0500

    Add registration serializers

---
+++ links/maker/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from maker.models import Maker
+
+
+class RegistrationRequestSerializer(serializers.Serializer):
+
+    identifier = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+
+
+class RegistrationResponseSerializer(serializers.Serializer):
+
+    id = serializers.IntegerField()
+    token = serializers.CharField()
