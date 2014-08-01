3b755180614d932e10af13d82b71eec5d8cf4a67
Brian Hines <brian@projectweekend.net>
2014-08-01 07:28:46 -0500

    Name urls for reverse

--- links/maker/urls.py
+++ links/maker/urls.py
@@ -10,18 +10,22 @@ urlpatterns = patterns(
     '',
     url(
         r'^/?$',
-        MakerSelfView.as_view()
+        MakerSelfView.as_view(),
+        name='maker-self'
     ),
     url(
         r'^register/?$',
-        RegsitrationView.as_view()
+        RegsitrationView.as_view(),
+        name='registration'
     ),
     url(
         r'^authenticate/?$',
-        AuthenticationView.as_view()
+        AuthenticationView.as_view(),
+        name='authentication'
     ),
     url(
         r'^password/?$',
-        ChangePasswordView.as_view()
+        ChangePasswordView.as_view(),
+        name='change-password'
     )
 )
