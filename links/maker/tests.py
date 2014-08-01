555927c8e4a596358960fb9469c93f59e9fcdc96
Brian Hines <brian@projectweekend.net>
2014-08-01 07:50:34 -0500

    Start writing tests

--- links/maker/tests.py
+++ links/maker/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+class RegistrationTest(TestCase):
+
+    url = reverse('registration')
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def testSuccess(self):
+        response = self.client.post(self.url, {
+            'identifier': 'something_cool',
+            'password': 'something secret',
+            'email': 'test@test.com',
+            'first_name': 'Testy',
+            'last_name': 'McTesterson'
+        }, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
