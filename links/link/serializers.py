b3dc4b5cb79344538f799ceaed9202f5f34a7770
Brian Hines <brian@projectweekend.net>
2014-08-03 13:28:12 -0500

    Add a serializer

---
+++ links/link/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from link.models import Link
+
+
+class LinkSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Link
+        fields = ('id', 'folder', 'title', 'url', 'note', 'photo_url', 'created')
+        read_only_fields = ('photo_url', 'created')
