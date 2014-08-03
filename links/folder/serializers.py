a24c080339834d6ceba2998844f1a11eee5851f2
Brian Hines <brian@projectweekend.net>
2014-08-03 14:51:16 -0500

    Add a detail serializer

--- links/folder/serializers.py
+++ links/folder/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
 from folder.models import Folder
+from link.serializers import LinkSerializer
 
 
 class FolderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Folder
+        fields = ('id', 'name', 'description', 'is_public', 'created',)
+        read_only_fields = ('created',)
+
+
+class FolderExtendedSerializer(serializers.ModelSerializer):
+
+    links = LinkSerializer(many=True)
 
     class Meta:
         model = Folder
