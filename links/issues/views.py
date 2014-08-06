9c688c759c0556d541564e91d2d3e099493242bb
Brian Hines <brian@projectweekend.net>
2014-08-06 05:19:12 -0500

    Issues views

--- links/issues/views.py
+++ links/issues/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from issues.mixins import (ReportedLinkAPI,
+                            ReportedLinkSelfAPI,
+                            ReportedUserAPI,
+                            ReportedUserSelfAPI)
+
+
+class ReportedLinkCreateView(ReportedLinkAPI, ReportedLinkSelfAPI,
+                                generics.CreateAPIView):
+    pass
+
+
+class ReportedUserCreateView(ReportedUserAPI, ReportedUserSelfAPI,
+                                generics.CreateAPIView):
+    pass
